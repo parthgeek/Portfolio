1138775abcdfe3764ab5bcd61768fb7d3f29973d
parthgeek <109090831+parthgeek@users.noreply.github.com>
2025-06-25 18:37:29 +0530

    intial commit

---
+++ src/app/consts/media.js
@@ -0,0 +1,16 @@
+import proxy from "../proxies/media"
+
+const media = {
+    discord: {
+       
+        tag: "parthsharma0134",
+    },
+    
+    github: "parthgeek",
+    
+    email: "sharmaprth364@gmail.com"
+
+}
+
+export default new Proxy(media, proxy);
+
---
+++ src/app/consts/skills.js
@@ -0,0 +1,8 @@
+// /consts/skills.js
+export default {
+  language: ["JavaScript", "TypeScript", "Java"],
+  database: ["MongoDB", "PostgreSQL"],
+  other: ["HTML", "CSS", "SASS", "Tailwindcss"],
+  tool: ["VSCode", "Figma", "Git","Supabase", "Postman"],
+  framework: ["React.js", "Next.js", "Express.js", "Node.js"]
+};
